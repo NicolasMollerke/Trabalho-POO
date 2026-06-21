b6bb89f20c3438e7907871449937ea16f4fd8ba7
Nicolas Mollerke <nicolasmollerke@gmail.com>
2026-06-21 18:25:35 -0300

    adiciona os métodos da classe Tabuleiro

--- src/trabalho/Tabuleiro.java
+++ src/trabalho/Tabuleiro.java
@@ -7,15 +7,21 @@ import java.util.Scanner;
 public class Tabuleiro {
     private Elemento[][] matriz;
     private int tamanho;
+    private Personagem jogador;
     
     public Tabuleiro (String caminhoArquivo, Personagem personagem) {
         this.tamanho = 10; 
         this.matriz = new Elemento[tamanho][tamanho]; 
+        this.jogador = personagem;
         
-        this.lerTabuleiro(caminhoArquivo, personagem);
+        this.lerTabuleiro(caminhoArquivo);
     }
     
-    private void lerTabuleiro (String caminhoArquivo, Personagem personagem) {
+    public Elemento[][] getMatriz() {
+        return matriz;
+    }
+    
+    private void lerTabuleiro (String caminhoArquivo) {
         try {
             File arquivo = new File(caminhoArquivo);
             Scanner scanner = new Scanner(arquivo);
@@ -27,28 +33,29 @@ public class Tabuleiro {
                         
                         switch (caractere) {
                             case "P": 
-                                this.matriz[i][j] = personagem;
+                                this.matriz[i][j] = jogador;
+                                jogador.setPosicao(i, j);
                                 break;
                             case "#":
-                                this.matriz[i][j] = new Parede();
+                                this.matriz[i][j] = new Parede(i, j);
                                 break;
                             case "T":
-                                this.matriz[i][j] = new Trodonte();
+                                this.matriz[i][j] = new Trodonte(i, j);
                                 break;
                             case "V":
-                                this.matriz[i][j] = new Velociraptor();
+                                this.matriz[i][j] = new Velociraptor(i, j);
                                 break;
                             case "C":
-                                this.matriz[i][j] = new Compsognato();
+                                this.matriz[i][j] = new Compsognato(i, j);
                                 break;
                             case "X":
-                                this.matriz[i][j] = new Kit(); // Perfeito! Mudou para Kit
+                                this.matriz[i][j] = new Kit(i, j);
                                 break;
                             case "R":
-                                this.matriz[i][j] = new Rex();
+                                this.matriz[i][j] = new Rex(i, j);
                                 break;
                             default:
-                                this.matriz[i][j] = null; // Garante que o "." vire espaço vazio
+                                this.matriz[i][j] = null;
                                 break;
                         }
                     }
@@ -62,9 +69,14 @@ public class Tabuleiro {
     }
     
     public void mostrarTabuleiro () {
-        System.out.print("  "); 
+        int x = jogador.linha;
+        int y = jogador.coluna;
+        boolean visivel[][] = this.campoDeVisao(x, y);
+        
+        System.out.print("  ");
         for (int col = 1; col <= 10; col++) System.out.print(col + " ");
         System.out.println();
+        
 
         for (int i = 0; i < tamanho; i++){
             System.out.print((char)('A' + i) + "| "); 
@@ -72,13 +84,78 @@ public class Tabuleiro {
             for (int j = 0; j < tamanho; j++){
                 Elemento atual = this.matriz[i][j];
             
-                if (atual == null) {
+                if (atual == null && visivel[i][j]) {
                     System.out.print(". "); 
-                } else {
+                } else if (atual != null && visivel[i][j]) {
                    System.out.print(atual.getSimbolo() + " ");
+                } else {
+                    System.out.print("? "); 
                 }
             }
             System.out.println();
         }
+        
+    }
+    
+    public boolean[][] campoDeVisao (int x, int y) {
+        boolean[][] visivel = new boolean [tamanho][tamanho];
+        visivel[x][y] = true;
+        
+        for (int l=x+1; l < tamanho; l++){
+            visivel[l][y] = true;
+            if (matriz[l][y] != null) {
+                visivel[l][y] = true;
+                break;
+            }
+        }
+        for (int o=x-1; o >= 0; o--){
+            visivel[o][y] = true;
+            if (matriz[o][y] != null) {
+                visivel[o][y] = true;
+                break;
+            }
+        }
+        
+        for (int n=y-1; n >= 0; n--){
+            visivel[x][n] = true;
+            if (matriz[x][n] != null) {
+                visivel[x][n] = true;
+                break;
+            }
+        }
+        
+        for (int s=y+1; s < tamanho; s++){
+            visivel[x][s] = true;
+            if (matriz[x][s] != null) {
+                visivel[x][s] = true;
+                break;
+            }
+        }
+        
+        return visivel;
+    }
+    
+    public int posicaoVertical (Elemento elemento) {
+        for (int i = 0; i < tamanho; i++){
+            for (int j = 0; j < tamanho; j++){
+                if (matriz[i][j] == elemento) {
+                    return i;
+                }
+            }
+        }
+    
+        return -1;
+    }
+    
+    public int posicaoHorizontal (Elemento elemento) {
+        for (int i = 0; i < tamanho; i++){
+            for (int j = 0; j < tamanho; j++){
+                if (matriz[i][j] == elemento) {
+                    return j;
+                }
+            }
+        }
+    
+        return -1;
     }
 }
