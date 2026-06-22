7773f041e0ee00affbeb4b0de54988a727fb9ffb
Nicolas Mollerke <nicolasmollerke@gmail.com>
2026-06-22 16:17:50 -0300

    implementa o método mover na classe Trodonte

--- src/trabalho/Trodonte.java
+++ src/trabalho/Trodonte.java
@@ -13,8 +13,51 @@ public class Trodonte extends Dinossauro{
         super(2, i, j);
     }
 
-    public void mover(Tabuleiro tabuleiro, Personagem personagem){
+    public void mover(Tabuleiro tabuleiro){
+        Personagem personagem = tabuleiro.getPersonagem();
+        Elemento[][] matriz = tabuleiro.getMatriz();
 
+        
+        int diferencaLinha = personagem.linha - this.linha;
+        int diferencaColuna = personagem.coluna - this.coluna;
+        
+        boolean mover = false;
+        int tentativa = 1;
+        
+        while (!mover && tentativa <= 2) {
+            int linhaNova = this.linha;
+            int colunaNova = this.coluna;
+            
+            if (tentativa == 1) {
+                if (Math.abs(diferencaColuna) > Math.abs(diferencaLinha)){
+                    if (diferencaColuna > 0) colunaNova++;
+                    else if (diferencaColuna < 0) colunaNova--;
+                } else {
+                    if (diferencaLinha > 0) linhaNova++;
+                    else if (diferencaLinha < 0) linhaNova--;
+                }
+            } else if (tentativa == 2) {
+                if (Math.abs(diferencaColuna) > Math.abs(diferencaLinha)){
+                    if (diferencaLinha > 0) linhaNova++;
+                    else if (diferencaLinha < 0) linhaNova--;
+                } else {
+                    if (diferencaColuna > 0) colunaNova++;
+                    else if (diferencaColuna < 0) colunaNova--;
+                }
+            }
+            
+            if (linhaNova >= 0 && linhaNova < tabuleiro.getTamanho() && colunaNova >=0 && colunaNova < tabuleiro.getTamanho()){
+                if (matriz[linhaNova][colunaNova] == null) {
+                    matriz[this.linha][this.coluna] = null;
+                    matriz[linhaNova][colunaNova] = this;
+                        
+                    this.linha = linhaNova;
+                    this.coluna = colunaNova;
+                        
+                        mover = true;
+                }            
+            }
+        }
     }
     
     public String getSimbolo() { 
