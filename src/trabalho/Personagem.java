bcc96cdd1de261e417d99c2d03afcc16d8883f91
Nicolas Mollerke <nicolasmollerke@gmail.com>
2026-06-22 15:24:02 -0300

    atualiza o método mover da classe Personagem

--- src/trabalho/Personagem.java
+++ src/trabalho/Personagem.java
@@ -34,52 +34,40 @@ public class Personagem extends Elemento{
     }
     
     public void moverJogador (Tabuleiro tabuleiro) {
-        System.out.println("Para onde voce quer se mover?");
-        System.out.println("1.^ Cima");
-        System.out.println("2.< Esquerda");
-        System.out.println("3.> Direita");
-        System.out.println("4.v Baixo");
-
         Scanner teclado = new Scanner(System.in);
-        int direcao = teclado.nextInt();
         Elemento[][] matriz = tabuleiro.getMatriz();
+        boolean mover = false;
+            
+        while (!mover) {    
 
-        if (direcao == 1) {
-            int linhaNova = this.linha - 1;
-            if (matriz[linhaNova][this.coluna] == null){
-                matriz[linhaNova][this.coluna] = this;
-                matriz[this.linha][this.coluna] = null;
-                setPosicao(linhaNova, this.coluna); 
-            }
-        }
-
-        // 2. ESQUERDA
-        if (direcao == 2) {
-            int colunaNova = this.coluna - 1;
-            if (matriz[this.linha][colunaNova] == null) {
-                matriz[this.linha][colunaNova] = this;
-                matriz[this.linha][this.coluna] = null;
-                setPosicao(this.linha, colunaNova);
-            }
-        }
-
-        if (direcao == 3) {
-            int colunaNova = this.coluna + 1;
-            if (matriz[this.linha][colunaNova] == null) {
-                matriz[this.linha][colunaNova] = this;
-                matriz[this.linha][this.coluna] = null;
-                setPosicao(this.linha, colunaNova);
-            }
-        }
-
-        if (direcao == 4) {
-            int linhaNova = this.linha + 1;
-            if (matriz[linhaNova][this.coluna] == null) {
-                matriz[linhaNova][this.coluna] = this;
-                matriz[this.linha][this.coluna] = null;
-                setPosicao(linhaNova, this.coluna);
+            System.out.println("Para onde voce quer se mover?");
+            System.out.println("1.^ Cima");
+            System.out.println("2.< Esquerda");
+            System.out.println("3.> Direita");
+            System.out.println("4.v Baixo"); 
+            
+            int direcao = teclado.nextInt();
+            
+            int linhaNova = this.linha;
+            int colunaNova = this.coluna;
+                
+            if (direcao == 1) linhaNova--; //Cima
+            else if (direcao == 2) colunaNova--; // Esquerda
+            else if (direcao == 3) colunaNova++; // Direita
+            else if (direcao == 4) linhaNova++;  // Baixo
+                
+            if (linhaNova >= 0 && linhaNova < tabuleiro.getTamanho() && colunaNova >=0 && colunaNova < tabuleiro.getTamanho()){
+                if (matriz[linhaNova][colunaNova] == null) {
+                    matriz[this.linha][this.coluna] = null;
+                    matriz[linhaNova][colunaNova] = this;
+                        
+                    this.linha = linhaNova;
+                    this.coluna = colunaNova;
+                        
+                    mover = true;
+                }
             }
         }
     }
-    
 }
+    
