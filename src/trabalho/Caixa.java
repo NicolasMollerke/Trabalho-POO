2e43bb9fb488780e3ea3599aa9648560d7e6edd2
Nicolas Mollerke <nicolasmollerke@gmail.com>
2026-06-24 12:57:18 -0300

    implementa a classe Caixa junto com seus métodos base

---
+++ src/trabalho/Caixa.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabalho;
+
+/**
+ *
+ * @author nicol
+ */
+public class Caixa extends Elemento{
+    public Caixa (int i, int j) {
+        super(i, j);
+    }
+    
+    public String getSimbolo() { 
+        return "X"; 
+    }
+}
